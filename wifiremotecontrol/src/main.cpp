4cc991059b59f952e37b66ef202165fbee4d6e9e
michi <a@a>
2020-07-09 09:49:32 +0200

    initial PlatformIO Project

---
+++ wifiremotecontrol/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "Rotary.h"
+
+Rotary rt(GPIO_NUM_19, GPIO_NUM_18);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
