e18522a1eea81d31f76631e049e8ad53c4d11387
cubicuz <no@mail.me>
2020-07-15 17:08:06 +0200

    [add] libs, Homeassistant

---
+++ wifiremotecontrol/include/Homeassistant.h
@@ -0,0 +1,20 @@
+#include <ArduinoJson.h>
+#include <MQTT.h>
+#include <WiFi.h>
+
+class Homeassistant
+{
+private:
+  /* data */
+public:
+  Homeassistant(/* args */);
+  ~Homeassistant();
+};
+
+Homeassistant::Homeassistant(/* args */)
+{
+}
+
+Homeassistant::~Homeassistant()
+{
+}
